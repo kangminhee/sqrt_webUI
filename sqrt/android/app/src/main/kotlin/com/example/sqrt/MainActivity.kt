34f97d3c1d4194c1a4ecc7aa6e60d0b65995ba5b
mingnei <minhee489@naver.com>
2023-11-18 00:46:55 +0900

    rm sqrt

--- sqrt/android/app/src/main/kotlin/com/example/sqrt/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.example.sqrt
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
--- sqrt/lib/models/food_list.dart
+++
@@ -1,9 +0,0 @@
-class FoodList {
-  final String name, price, image, id;
-
-  FoodList.fromJson(Map<String, dynamic> json)
-      : id = json['id'],
-        name = json['name'],
-        price = json['price'],
-        image = json['image'];
-}
